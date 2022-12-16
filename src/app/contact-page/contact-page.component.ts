9fc85c5246204efa209030e6de7fdcf93d62c91d
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2022-12-16 20:28:40 +0530

    implements some components

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  styleUrls: ['./contact-page.component.css'],
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing: Listing | undefined;
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.message = this.listing
+      ? `Hi! i'm intrested in your ${this.listing.name.toLowerCase()}!`
+      : '';
+  }
+
+  sendMessage(): void {
+    alert("Message has been sent!");
+    console.log(this.email);
+    console.log(this.message);
+    this.router.navigateByUrl('/listings')
+  }
 }
